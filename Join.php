f64049338e47709d5a6f134304b96145302d2d7c
얌챠 <dbals9812@naver.com>
2019-12-05 22:07:22 +0900

    added memberNum on join.php

--- Join.php
+++ Join.php
@@ -2,6 +2,7 @@
 header ("Content-type:text/html; charset=utf-8");
 $HTTP_REFERER=$_SERVER['HTTP_REFERER'];
 
+$memberNum = 0;
 $Email = $_POST["Email"];
 $password = $_POST["password"];
 $name = $_POST["name"];
@@ -12,22 +13,16 @@ $myfile = fopen("./data/busMember.txt", "a+") or die("Unable to open file!");
 while (!feof($myfile)){
     $data = fgets($myfile);
     $indata = explode('|',$data);
+    $memberNum++;
     if(strcmp($Email,$indata[0])==0){
       echo "이메일이 중복됩니다. 다시 회원 가입해주세요. ";
       return ;
     }
 }
 
-fwrite($myfile, $Email);
-fwrite($myfile, "|");
-fwrite($myfile, $password);
-fwrite($myfile, "|");
-fwrite($myfile, $name);
-fwrite($myfile, "|");
-fwrite($myfile, $phoneNum);
-fwrite($myfile,"\n");
+fwrite($myfile, "{$Email}|{$password}|{$name}|{$phoneNum}|{$memberNum}\n");
 fclose($myfile);
 
-echo "회원가입이 완료되었습니다."
+echo "회원가입이 완료되었습니다!"
 
 ?>
--- login.js
+++ login.js
@@ -1,116 +1,104 @@
+function vaildcheckSignin() {
+  var checkEmail = false;
+  var checkPassword = false;
+  var validEmail = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+  var inputEmail = document.getElementById("Email").value;
+  var vaildPassword = /^.*(?=^.{8,15}$)(?=.*|d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
+  var inputPassword = document.getElementById("pw").value;
+  var inputName = document.getElementById('name').value;
+  var inputPhnum = document.getElementById('phoneNum').value;
 
+  if (inputEmail.match(validEmail)) {
+    checkEmail = true;
+  }
 
-function vaildcheckSignin(){
-var checkEmail = false;
-var checkCheck = false;
-var checkPassword = false;
-var validEmail =  /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
-var inputEmail = document.getElementById("Email").value;
-var vaildPassword = /^.*(?=^.{8,15}$)(?=.*|d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
-var inputPassword = document.getElementById("pw").value;
-var inputName = document.getElementById('name').value;
-var inputPhnum = document.getElementById('phoneNum').value;
+  if (inputPassword.match(vaildPassword)) {
+    checkPassword = true;
+  }
 
-if(inputEmail.match(validEmail)) {
-		checkEmail = true;
-}
+  var submitData = "";
+  var email = document.getElementsByClassName("Email");
+  var password = document.getElementsByName("pw");
+  var name = document.getElementsByName("name");
+  var phoneNum = document.getElementsByName('phoneNum');
 
-if(inputPassword.match(vaildPassword)){
-		checkPassword=true;
-}
+  if (inputEmail == "" || inputPassword == "" || inputName == "" || inputPhnum == "") {
+    alert("빈칸을 남기지 말고 다 입력해주세요.");
+  } else if (checkEmail === false || checkPassword === false) {
+    alert("이메일 또는 패스워드의 입력양식을 체크해주세요.");
+  } else {
+    var submitData = "";
+    submitData = document.getElementById("Email").value;
 
-var submitData = "";
-var email = document.getElementsByClassName("Email");
-var password = document.getElementsByName("pw");
-var name = document.getElementsByName("name");
-var phoneNum = document.getElementsByName('phoneNum');
-
-if(inputEmail == "" || inputPassword =="" || inputName ==""||inputPhnum==""){
-		alert ("빈칸을 남기지 말고 다 입력해주세요.");
-}
-
-else if(checkEmail === false || checkPassword === false) {
-		alert("이메일 또는 패스워드의 입력양식을 체크해주세요.");
-
-}
-
-else {
-		var submitData = "";
-		submitData = document.getElementById("Email").value;
-
-		var xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function(){
-			if(this.readyState==4&&this.status==200){
-				alert(this.responseText);
-			}
-		};
-		xhttp.open("POST", "join.php", true);
-		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhttp.send("Email="+inputEmail+"&password="+inputPassword+"&name="+inputName+"&phoneNum="+inputPhnum);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        alert(this.responseText);
+      }
+    };
+    xhttp.open("POST", "join.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("Email=" + inputEmail + "&password=" + inputPassword + "&name=" + inputName + "&phoneNum=" + inputPhnum);
   }
 }
 
-function vaildcheckSumit(){
+function vaildcheckSumit() {
   var checkEmail = false;
   var checkCheck = false;
   var checkPassword = false;
-  var validEmail =  /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+  var validEmail = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
   var inputEmail = document.getElementById("Email").value;
   var vaildPassword = /^.*(?=^.{8,15}$)(?=.*|d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
   var inputPassword = document.getElementById("pw").value;
 
-  if(inputEmail.match(validEmail)) {
-  		checkEmail = true;
+  if (inputEmail.match(validEmail)) {
+    checkEmail = true;
   }
 
-  if(inputPassword.match(vaildPassword)){
-  		checkPassword=true;
+  if (inputPassword.match(vaildPassword)) {
+    checkPassword = true;
   }
 
   var submitData = "";
   var email = document.getElementsByClassName("Email");
   var password = document.getElementsByName("pw");
 
-  if(inputEmail == "" || inputPassword ==""){
-  		alert ("아이디 또는 패스워드의 입력양식을 체크해주세요");
+  if (inputEmail == "" || inputPassword == "") {
+    alert("아이디 또는 패스워드의 입력양식을 체크해주세요");
+  } else if (checkEmail === false || checkPassword === false) {
+    alert("아이디 또는 패스워드의 입력양식을 체크해주세요.");
+
+  } else {
+    var submitData = "";
+    submitData = document.getElementById("Email").value;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        if (this.responseText == inputEmail) {
+          sessionStorage.setItem('name', document.getElementById('Email').value);
+          // schedulerCheck();
+          var x = document.getElementById("idDiv");
+          var t = document.createTextNode(this.responseText);
+          x.appendChild(t);
+          // document.body.appendChild(x);
+        }
+      }
+    };
+    xhttp.open("POST", "login.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("Email=" + inputEmail + "&password=" + inputPassword);
   }
-
-  else if(checkEmail === false || checkPassword === false) {
-  		alert("아이디 또는 패스워드의 입력양식을 체크해주세요.");
-
 }
 
-else {
-		var submitData = "";
-		submitData = document.getElementById("Email").value;
-
-		var xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function(){
-			if(this.readyState==4&&this.status==200){
-				if(this.responseText == inputEmail){
-				sessionStorage.setItem('name', document.getElementById('Email').value);
-				// schedulerCheck();
-				var x = document.getElementById("idDiv");
-				var t = document.createTextNode(this.responseText);
-				x.appendChild(t);
-				// document.body.appendChild(x);
-		   }
-			}
-		};
-		xhttp.open("POST", "login.php", true);
-		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhttp.send("Email="+inputEmail+"&password="+inputPassword);
-}
-}
-
-function backtologin(){
-	var inputEmail = document.getElementById("Email");
-	var inputPassword = document.getElementById("pw");
-	var inputName = document.getElementById('name');
-	var inputPhnum = document.getElementById('phoneNum');
-	inputEmail.value ="";
-	inputName.value ="";
-	inputPhnum.value ="";
+function backtologin() {
+  var inputEmail = document.getElementById("Email");
+  var inputPassword = document.getElementById("pw");
+  var inputName = document.getElementById('name');
+  var inputPhnum = document.getElementById('phoneNum');
+  inputEmail.value = "";
+  inputName.value = "";
+  inputPhnum.value = "";
   window.history.back();
 
 }

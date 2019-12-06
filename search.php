94727f027d63b3041bf2b8f9c7eadbdaefe4674a
얌챠 <dbals9812@naver.com>
2019-12-06 19:46:44 +0900

    Update search.php

--- search.php
+++ search.php
@@ -1,23 +1,20 @@
 <?php
-
 $to = $_POST["to"];
 $from = $_POST["from"];
+//현재 날짜와 시간까지 받아와서
 
-$myfile = fopen("./data/busData.txt", "a+") or die("Unable to open file!");
-while (!feof($myfile)){
-  $line = fgets($myfile);
-  $indata = explode('|',fgets($myfile));
+$busFile = fopen("./data/busData.txt", "a+") or die("Unable to open file!");
+while (!feof($busFile)){
+  $line = fgets($busFile);
+  $indata = explode('|',fgets($busFile));
 
-  // if((!strcmp($Email,$indata[0])) && (!strcmp($pw,$indata[1]))){
-  //   echo $indata[2];
-  //   return ;
-  // }else {
-  //   echo "nothing";
-  // }
 
+  if((!strcmp($to,$indata[2])) && (!strcmp($from,$indata[3]))){
+    if (시간이랑 날짜 비교) {
+      //비교를 할때 출발지 목적지 뿐만 아니라 날짜와 시간까지 보고 해당하는 버스를 골라냄
+      echo "{$indata[0]}|{$indata[1]}|{$indata[2]}|{$indata[3]}|{$indata[4]}|{$indata[5]}$";
+    }
   }
-  fwrite("");
-  fclose($myfile);
-  echo "ss";
-
+}
+fclose($busFile);
 ?>

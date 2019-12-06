17197a5af2ab38c9d31b29db7d1ec0c553ce7131
얌챠 <dbals9812@naver.com>
2019-12-06 19:59:38 +0900

    Update searchBus

--- search.php
+++ search.php
@@ -1,17 +1,17 @@
 <?php
 $to = $_POST["to"];
 $from = $_POST["from"];
-//현재 날짜와 시간까지 받아와서
+//???현재 날짜와 시간까지 받아와서
 
 $busFile = fopen("./data/busData.txt", "a+") or die("Unable to open file!");
 while (!feof($busFile)){
   $line = fgets($busFile);
   $indata = explode('|',fgets($busFile));
 
-
   if((!strcmp($to,$indata[2])) && (!strcmp($from,$indata[3]))){
-    if (시간이랑 날짜 비교) {
-      //비교를 할때 출발지 목적지 뿐만 아니라 날짜와 시간까지 보고 해당하는 버스를 골라냄
+    if (true) {
+      //???비교를 할때 출발지 목적지 뿐만 아니라
+      //날짜와 시간까지 보고 해당하는 버스를 전부 echo
       echo "{$indata[0]}|{$indata[1]}|{$indata[2]}|{$indata[3]}|{$indata[4]}|{$indata[5]}$";
     }
   }
--- searchBus.js
+++ searchBus.js
@@ -12,7 +12,7 @@ function btnClick() {
   } else if (busFrom == "") {
     alert("목적지를 입력해 주세요");
   } else {
-    //제대로 값이 입력되었고 이 값들을 BusInfo.txt에서 검사한다
+    //제대로 값이 입력되었고 이 값들을 busData.txt에서 검사한다
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
@@ -20,15 +20,37 @@ function btnClick() {
         while (tbodyToAppend.hasChildNodes()) {
           tbodyToAppend.removeChild(tbodyToAppend.firstChild);
         }
-        //자식 요소를 전부 제거하고
+        //자식 요소를 전부 제거
 
+        var busSearchResultArray = this.responseText.split("$");
+        //php에서 가져온 조건에 맞는 버스들의 배열
+        for (var i = 0; i < busSearchResultArray.length; i++) {
+          var trNode = document.createElement("tr");
+          trNode.setAttribute("class", "busList");
 
-        //조건에 맞는 버스 요소를 만들어 append해준다
+          var busDetailArray = busSearchResultArray[i].split("|");
+          for (var i = 0; i < busDetailArray.length; i++) {
+            var tdNode = document.createElement("td");
+            var txtNode = document.createTextNode(busDetailArray[i]);
+            tdNode.append(txtNode);
+            trNode.append(tdNode);
+          }
+          //버스 정보를 갖는 tr만드는 반복문
+          /* 양식 이거임
+          <tr class="busList">
+            <td class="busNum">버스번호</td>
+            <td>버스번호</td>
+            <td>버스번호</td>
+            <td>버스번호</td>
+          </tr>
+          */
 
-        alert(this.responseText + "님 환영합니다.");
+          tbodyToAppend.append(trNode);
+        }
       }
     };
     xhttp.open("POST", "search.php", true);
     xhttp.send("to=" + busTo + "&from=" + busFrom);
+    //???시간도 같이 넘겨주기
   }
 }

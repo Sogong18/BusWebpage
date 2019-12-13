74352e4cb80d011471dc1be8572b3f96a50b4a18
얌챠 <dbals9812@naver.com>
2019-12-13 21:54:56 +0900

    todos

--- Payment_done.js
+++ Payment_done.js
@@ -1,6 +1,24 @@
 window.addEventListener('DOMContentLoaded', function() {
-  // document.getElementById("amount").innerText = (((window.location.href).split("?"))[1]).split(":")[1];
-  //버스정보 업데이트할부분
+  var recivedBusNum = (((window.location.href).split("?"))[1]).split(":")[1];
+  //주소에서 버스 번호를 가져옴
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      if (this.responseText != "openError") {
+        var busDetailDataArray = this.responseText.split("|");
+        document.getElementById("busID").innerText = recivedBusNum;
+        document.getElementById("departDay").innerText = busDetailDataArray[0];
+        document.getElementById("place").innerText = busDetailDataArray[1]+"-"+busDetailDataArray[2];
+        document.getElementById("departTime").innerText = busDetailDataArray[3];
+        // document.getElementById("Time_required").innerText = busDetailDataArray[4];
+      } else {
+        alert("버스 정보 파일을 로드할 수 없습니다.");
+      }
+    }
+  };
+  xhttp.open("POST", "busLoad.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("busNum=" + recivedBusNum);
 });
 
 function no(){
